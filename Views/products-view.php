f356ca16a0565bfa96be0234ac77db79b9f96af9
Shweta Prasad <SPrasad@vanarts.com>
2018-10-27 13:02:09 -0700

    product view check

---
+++ Views/products-view.php
@@ -0,0 +1,14 @@
+<div class="productsView">
+		<div class="categoryNav">
+			<ul>
+				<li><a href="#">Categories</a>
+					<ol><a href="#">Dog Beds</a></ol>
+					<ol><a href="#">Accessories</a></ol>
+				</li><!--categories-->
+				<li><a href="#">Size</a></li>
+				<li><a href="#">Waterproof</a></li>
+			</ul>
+		</div>
+	<div class="viewProducts">
+	</div>
+</div>
