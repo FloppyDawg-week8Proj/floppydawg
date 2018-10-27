2b361429436dcef1e8a2e77510bd532343acb175
Shweta Prasad <SPrasad@vanarts.com>
2018-10-27 14:23:13 -0700

    products view commit one

---
+++ Views/products-view.php
@@ -0,0 +1,18 @@
+<?php include("header-view.php");?>
+<div class="productsView">
+		<div class="categoryNav">
+			<ul>
+				<h3>CATEGORIES AND FILTER</h3>
+				<li><a href="#" class="dropBtn" onClick="dropBtn()">Categories</a>
+					<div id="dropBtn" class="dropbtn-content">
+						<li><a href="#">Dog Beds</a></li>
+						<li><a href="#">Accessories</a></li>
+					</div>
+				</li><!--categories-->
+				<li><a href="#">Size</a></li>
+				<li><a href="#">Waterproof</a></li>
+			</ul>
+		</div><!--categoryNav-->
+	<div class="viewProducts">
+	</div><!--viewProducts-->
+</div><!--productsView-->
