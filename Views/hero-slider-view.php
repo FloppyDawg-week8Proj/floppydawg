145e90cb3a78fc26f6797f9834c0466304e67b29
VANARTS\icarillo <italo.carillo@globo.com>
2018-10-27 00:19:48 -0700

    fixing conflict

---
+++ Views/hero-slider-view.php
@@ -0,0 +1,47 @@
+<?php include("header-view.php"); ?>
+<section class="heroSlider">
+	<div class="slide-keyhole container">
+		<div class="slide-container">
+			<div class="slide-holder">
+				<span class="slide-imgWraper">
+					<div class="slide-imgHolder coverBg">
+						<img width="200" src="../assets/placeholder.jpg" alt="default" class="slide-img">
+					</div>	
+				</span>	
+			</div>
+			<div class="slide-holder">
+				<span class="slide-imgWraper">
+					<div class="slide-imgHolder coverBg">
+						<img width="200" src="../assets/placeholder2.jpg" alt="default" class="slide-img">
+					</div>	
+				</span>	
+			</div>
+			<div class="slide-holder">
+				<span class="slide-imgWraper">
+					<div class="slide-imgHolder coverBg">
+						<img width="200" src="../assets/placeholder.jpg" alt="default" class="slide-img">
+					</div>	
+				</span>	
+			</div>
+			<div class="slide-holder">
+				<span class="slide-imgWraper">
+					<div class="slide-imgHolder coverBg">
+						<img width="200" src="../assets/placeholder2.jpg" alt="default" class="slide-img">
+					</div>	
+				</span>	
+			</div>
+			<div class="slide-holder">
+				<span class="slide-imgWraper">
+					<div class="slide-imgHolder coverBg">
+						<img width="200" src="../assets/placeholder.jpg" alt="default" class="slide-img">
+					</div>	
+				</span>	
+			</div>
+		</div>
+		<span class="slide-control slide-back">LEFT ARROW</span>
+		<span class="slide-control slide-next">RIGHT ARROW</span>
+	</div>
+</section>
+<script  src="https://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+<script src="../js/makeCoverBg.js"></script>
+<script src="../js/heroSlider.js"></script>
