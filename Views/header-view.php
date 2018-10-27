c31069f79db77d5963f5b8db22ad962a8063f445
Shweta Prasad <SPrasad@vanarts.com>
2018-10-26 21:34:40 -0700

    header-view first commit

---
+++ Views/header-view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto+Slab" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<a href="#"><img src="images/LogoPlaceholder.png" alt="logo" ></a>
+		</div><!--logo-->
+		<div class="headerNav">
+			<nav>
+				<ul class="mainNav">
+				  <li><a href="#">Home</a></li>
+				  <li class="dropdown">
+				    <a href="#" class="dropbtn">Products</a>
+				    <div class="dropdownContent">
+				      <a href="#">Product Categories</a>
+				    </div><!--drop down content-->
+				  </li><!--dropdown-->
+				  <li><a href="#">About</a></li>
+				  <li><a href="#">Contact</a></li>
+				</ul>
+			</nav><!--Navigation-->
+		</div>
+
+		<div class="rightNav">
+			<div class="socialNav">
+				<a href="#"><img src="images/fb.png" alt="facebook" class="socialIcon"></a>
+				<a href="#"><img src="images/insta.png" alt="instagram" class="socialIcon"></a>
+			</div><!--socialNav-->
+
+			<div class="cartNav">
+				<a href="">Login</a>
+				<a href="#"><img src="images/user.png" alt="user" class="user"></a>
+				<a href="">Cart</a>
+				<a href="#"><img src="images/cart.png" alt="cart" class="cart"></a>
+			</div><!--cartNav-->
+		</div><!--rightNav-->
+	</header>
+</body>
+</html>
