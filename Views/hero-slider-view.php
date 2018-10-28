66bfea03844beb8466ead0e7565fbd2b619fde0d
VANARTS\icarillo <italo.carillo@globo.com>
2018-10-28 14:51:57 -0700

    controller working

--- Views/hero-slider-view.php
+++ Views/hero-slider-view.php
@@ -1,43 +1,43 @@
 <section class="heroSlider">
-	<div class="slide-keyhole container">
+	<div class="slide-keyhole">
 		<div class="slide-container">
 			<div class="slide-holder">
 				<span class="slide-imgWraper">
 					<div class="slide-imgHolder coverBg">
-						<img width="200" src="../assets/placeholder.jpg" alt="default" class="slide-img">
+						<img width="200" src="assets/placeholder.jpg" alt="default" class="slide-img">
 					</div>	
 				</span>	
 			</div>
 			<div class="slide-holder">
 				<span class="slide-imgWraper">
 					<div class="slide-imgHolder coverBg">
-						<img width="200" src="../assets/placeholder2.jpg" alt="default" class="slide-img">
+						<img width="200" src="assets/placeholder2.jpg" alt="default" class="slide-img">
 					</div>	
 				</span>	
 			</div>
 			<div class="slide-holder">
 				<span class="slide-imgWraper">
 					<div class="slide-imgHolder coverBg">
-						<img width="200" src="../assets/placeholder.jpg" alt="default" class="slide-img">
+						<img width="200" src="assets/placeholder.jpg" alt="default" class="slide-img">
 					</div>	
 				</span>	
 			</div>
 			<div class="slide-holder">
 				<span class="slide-imgWraper">
 					<div class="slide-imgHolder coverBg">
-						<img width="200" src="../assets/placeholder2.jpg" alt="default" class="slide-img">
+						<img width="200" src="assets/placeholder2.jpg" alt="default" class="slide-img">
 					</div>	
 				</span>	
 			</div>
 			<div class="slide-holder">
 				<span class="slide-imgWraper">
 					<div class="slide-imgHolder coverBg">
-						<img width="200" src="../assets/placeholder.jpg" alt="default" class="slide-img">
+						<img width="200" src="assets/placeholder.jpg" alt="default" class="slide-img">
 					</div>	
 				</span>	
 			</div>
 		</div>
-		<span class="slide-control slide-back">LEFT ARROW</span>
-		<span class="slide-control slide-next">RIGHT ARROW</span>
+		<span class="slide-control slide-back fas fa-angle-left"></span>
+		<span class="slide-control slide-next fas fa-angle-right"></span>
 	</div>
 </section>
--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
+//include("Libs/DBFact.php");
+//include("Libs/DB.php");
+//include("Libs/Cart.php");
+//include("Models/Categories.php");
+//include("Models/Products.php");
+//include("Models/Product.php");
+//include("Models/Category.php");
+include("Controller/MainController.php");
 
-$pageOnWork = "history-view";
+session_start();
+
+$controller = (isset($_GET["controller"]))?$_GET["controller"]:"main";
+$action = (isset($_GET["action"]))	?$_GET["action"]:"home";
+
+$controllerName = ucfirst($controller)."Controller"; // creates MainController
+$oController = new $controllerName(); // new MainController();
+
+$oController->$action();
+
+?>
+<!--
 
 include("Views/header-view.php");
 
 include("Views/".$pageOnWork.".php");
 
-include("Views/footer-view.php");
-
-?>
+include("Views/footer-view.php");-->
