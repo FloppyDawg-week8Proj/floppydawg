7859b137e3a11820fa68238ca4807895727b7b97
Shweta Prasad <SPrasad@vanarts.com>
2018-10-27 18:11:51 -0700

    aboutView commit one

---
+++ Views/about-view.php
@@ -0,0 +1,44 @@
+<?php include("header-view.php");?>
+<style type="text/css">
+	.aboutContainer>*
+	{
+		display: inline-block;
+	}
+	.aboutContainer
+	{
+		border: 1px solid #363636;
+		background-color: #c1b8b5;
+		height: 400px;
+		padding: 20px;
+		margin: 0 auto;
+		max-width: 800px;
+		margin-top: 10%;
+	}
+	.aboutContainer img
+	{
+		float: left;
+		max-width: 400px;
+		margin-right: 20px;
+	}
+	.aboutContainer p
+	{
+		float: right;
+		font-size: 20px;
+		font-family: 'Montserrat', sans-serif;
+		color: #363636;
+	}
+	.aboutContainer h2
+	{
+		font-family: 'Roboto Slab', serif;
+		text-align: center;
+		margin-bottom: 20px;
+		color: #363636;
+	}
+</style>
+<div class="aboutContainer">
+	<h2>About Us</h2>
+		<p><img src="https://hlfppt.org/wp-content/uploads/2017/04/placeholder.png" alt="placeholder">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
+		Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+		</p>
+</div><!--aboutContainer-->
