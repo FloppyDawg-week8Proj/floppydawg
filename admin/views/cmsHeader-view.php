325cbb21db8ef565860dcac0c3c36515ba81ed2c
Abigail Ferguson <AFerguson@vanarts.com>
2018-10-29 23:22:46 -0700

    created cms header

---
+++ admin/views/cmsHeader-view.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="../css/style.css">
+<header id="cmsSideNav">
+<img src="../images/LogoPlaceholder.png" alt="LogoPlaceholder" class="cmsLogo">
+
+<nav>
+	<ul>
+		<a href=""><li>Products</li></a>
+		<a href=""><li>Pages</li></a>
+		<a href=""><li>Filters</li></a>
+		<a href=""><li>Admin Settings</li></a>
+		<a href=""><li>Log out</li></a>
+	</ul>
+</nav>
+</header><!--cmsSideNav-->
