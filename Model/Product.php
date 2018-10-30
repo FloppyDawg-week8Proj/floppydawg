c81a8cbfaddd4a0768eb24a764e8f20ffa5c4943
Shweta Prasad <SPrasad@vanarts.com>
2018-10-29 18:03:20 -0700

    insert and update for products commit one

--- Model/Product.php
+++ Model/Product.php
@@ -7,8 +7,51 @@ Class Product
 		return DB::getRecord()->runSQL("getSingleData", $sql);
 	}
 	static function saveNew($arrNewProduct){
-//		$sql = "SELECT * FROM products WHERE id=$productID";    SHWETAAAAAAAAAAAAAAAA!!!!! INSERT the right sql HERE!!!!!	
+		$sql = "INSERT INTO products(
+			'strName',
+			'nPrice',
+			'nCategory',
+			'strDescription',
+			'strAdvantage',
+			'strImage',
+			'nImageID',
+			'nReviews',
+			'nStars',
+			'nSizeID',
+			'bFeatures')
+		VALUES ('
+			".$arrNewProduct['strName'].",
+			".$arrNewProduct['nPrice'].",
+			".$arrNewProduct['nCategory'].",
+			".$arrNewProduct['strDescription'].",
+			".$arrNewProduct['strAdvantage'].",
+			".$arrNewProduct['strImage'].",
+			".$arrNewProduct['nImageID'].",
+			".$arrNewProduct['nReviews'].",
+			".$arrNewProduct['nStars'].",
+			".$arrNewProduct['nSizeID'].",
+			".$arrNewProduct['bFeatures'].")";
+//		$sql = "SELECT * FROM products WHERE id=$produc,tID";    SHWETAAAAAAAAAAAAAAAA!!!!! INSERT the right sql HERE!!!!!	
 		return DB::getRecord()->runSQL("insertNew", $sql);
+	}
+		static function edit($arrEditProduct){
+		$sql = "UPDATE
+					products
+				SET 
+					'strName'='".$arrEditProduct["strName"]."',
+					'nPrice'='".$arrEditProduct["nPrice"]."',
+					'nCategory'='".$arrEditProduct["nCategory"]."',
+					'strDescription'='".$arrEditProduct["strDescription"]."',
+					'strAdvantage'='".$arrEditProduct["strAdvantage"]."',
+					'strImage'='".$arrEditProduct["strImage"]."',
+					'nImageID'='".$arrEditProduct["nImageID"]."',
+					'nReviews'='".$arrEditProduct["nReviews"]."',
+					'nStars'='".$arrEditProduct["nStars"]."',
+					'nSizeID'='".$arrEditProduct["nSizeID"]."',
+					'bFeatures='".$arrEditProduct["bFeatures"].",
+				WHERE products.id = ".$arrEditProduct['id']."";
+//		$sql = "SELECT * FROM products WHERE id=$produc,tID";    SHWETAAAAAAAAAAAAAAAA!!!!! INSERT the right sql HERE!!!!!	
+		return DB::getRecord()->runSQL("edit", $sql);
 	}
 	static function delete($productID){
 		$sql = "DELETE FROM products WHERE id=$productID";
