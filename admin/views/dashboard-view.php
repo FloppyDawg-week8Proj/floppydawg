b45e3adcd4d6272e5e91b12fc44619d782cd37a6
Shweta Prasad <SPrasad@vanarts.com>
2018-10-31 01:54:18 -0700

    conflictResolveProduct

--- admin/views/dashboard-view.php
+++ admin/views/dashboard-view.php
@@ -9,27 +9,22 @@ include("cmsHeader-view.php");
 
 <div class="dashboardOptions">
 <span>
-	<a href=""><img src="images/iconBox" alt="iconBox" class="iconBox">
+	<a href=""><img src="../images/iconBox" alt="iconBox" class="iconBox">
 	<p class="productsOption">Products</p>
 	</a>
 </span>
 
-<<<<<<< HEAD
 <span>
-	<a href=""><img src="images/iconPages" alt="iconPages" class="iconPages">
-=======
-<!-- <span>
 	<a href=""><img src="../images/iconPages" alt="iconPages" class="iconPages">
->>>>>>> 1e51e2ef0197d5ecb308ba0527efbe6b9c20d33e
 	<p>Pages</p>
 	</a>
 </span>
 
 <span>
-	<a href=""><img src="images/iconFilter" alt="iconFilter" class="iconFilter">
+	<a href=""><img src="../images/iconFilter" alt="iconFilter" class="iconFilter">
 	<p>Filters</p>
 	</a>
-</span> -->
+</span>
 </div><!--dashboardOptions-->
 </section>
 
--- admin/views/products-view.php
+++ admin/views/products-view.php
@@ -24,15 +24,13 @@ $arrProducts = runSelectSQL("SELECT categories.strName as category, products.*, 
 	<tr>
 		<td><?=$products['strName']?></td>
 		<td><?=$products['category']?></td>
-<<<<<<< HEAD
 		<td><img width=200 src="../assets/<?=$products['strFile']?>"></td>
 		<td><a href="?page=editProducts&id=<?=$products['id'];?>" class="editBtn">Edit</a></td>
 		<td><a href="?page=deleteProduct&id=<?=$products['id'];?>" class="deleteBtn">Delete</a></td>
-=======
 		<td><img src="../../assets/<?=$products['strFile']?>"" alt="productImg"  width="200"></td>
 		<td><a href="" class="editBtn">Edit</a></td>
 		<td><a href="" class="deleteBtn">Delete</a></td>
->>>>>>> 1e51e2ef0197d5ecb308ba0527efbe6b9c20d33e
+
 	</tr>
 <?php } ?>
 
