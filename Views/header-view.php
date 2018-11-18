39e7bad0bab6715f81e0e02499c01046e3df07b9
Shweta Prasad <SPrasad@vanarts.com>
2018-11-17 23:17:23 -0800

    headerDesign

--- Views/header-view.php
+++ Views/header-view.php
@@ -9,39 +9,39 @@
 </head>
 <body>
 	<header>
-		<div class="logo coverBg">
-			<a href="index.php"><img src="images/LogoPlaceholder.png" alt="logo" ></a>
-		</div><!--logo-->
-		<div class="headerNav">
-			<nav>
-				<ul class="mainNav">
-				  <li><a href="index.php">Home</a></li>
-				  <li>
-				    <a href="index.php?action=products">Products</a>
-				    <ul class="dropdown">
-				    	<li>
-				      		<a href="index.php?action=products&categoryID=1">Beds</a>
-						</li>
-			      		<li>
-				      		<a href="index.php?action=products&categoryID=2">Acessories</a>
-						</li>
-				    </ul><!--drop down content-->
-				  </li><!--dropdown-->
-				  <li><a href="index.php?action=about">About</a></li>
-				  <li><a href="index.php?action=contact">Contact</a></li>
-				</ul>
-			</nav><!--Navigation-->
+		<div class="container">
+			<div class="logo coverBg">
+				<a href="index.php"><img src="images/floppyLogo.png" alt="logo" ></a>
+			</div><!--logo-->
+			<div class="headerNav">
+				<nav>
+						<ul class="mainNav">
+						  <li><a href="index.php">Home</a></li>
+						  <li><a href="index.php?action=products">Products</a>
+						    <ul class="dropdown">
+						    	<li><a href="index.php?action=products&categoryID=1">Beds</a>
+								</li>
+					      		<li><a href="index.php?action=products&categoryID=2">Acessories</a>
+								</li>
+						    </ul><!--drop down content-->
+						  </li><!--dropdown-->
+						  <li><a href="index.php?action=about">About</a></li>
+						  <li><a href="index.php?action=contact">Contact</a></li>
+						</ul>
+				</nav><!--Navigation-->
+			</div>
+
+			<div class="rightNav">
+				<div class="cartNav">
+					<a href="index.php?controller=cart&action=showitems"><span class="fas fa-shopping-cart"></span>Cart(<?=isset($_SESSION['cartProducts'])?count($_SESSION['cartProducts']):0;?>)</a>
+				</div><!--cartNav-->
+				<div class="userNav">
+					<a href=""><span class="fas fa-user"></span>Account</a>
+				</div><!--cartNav-->
+			</div><!--rightNav-->
 		</div>
-
-		<div class="rightNav">
-			<div id="socialMedia"> <!-- here we will have the icons for social medias -->			
-			    <span id="facebookIcon" class="socialSprite"><a href="#">Facebook</a></span>
-			    <span id="instagramIcon" class="socialSprite"><a href="#">Instagram</a></span>
-			</div><!--- closing div for social icons -->
-
-
-			<div class="cartNav">
-				<a href="index.php?controller=cart&action=showitems">Cart <span class="fas fa-shopping-cart"></span> (<?=isset($_SESSION['cartProducts'])?count($_SESSION['cartProducts']):0;?>)</a>
-			</div><!--cartNav-->
-		</div><!--rightNav-->
+		<div class="bottomLine">
+			<p>FREE SHIPPING (NO MINIMUM)</p>
+		</div><!--bootomLine-->
 	</header>
+
