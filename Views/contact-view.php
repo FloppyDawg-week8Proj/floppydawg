e5c6cf38cdf59daa4050c0d6eb2e83244af30e1b
Abigail Ferguson <AFerguson@vanarts.com>
2018-11-18 23:27:19 -0800

    css clean on about, contact css

--- Views/contact-view.php
+++ Views/contact-view.php
@@ -4,28 +4,27 @@
 		<div class="addressView">
 			<div class="addressDetails">
 				<h2>Address</h2>
-				<p>123/4A,Vancouver, Denman, P0S 7AL</p>
+				<p>123 Dallas, TX 70123</p>
 
-				<h2>Mail:</h2>
-				<p>contact@email.com</p>
+				<h2>Email</h2>
+				<p>info@floppwdawg.com</p>
 
-				<h2>FAQ Info:</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
-			</div><!--addressDetails--->
-
+				<h2>FAQ Info</h2>
+				<p>Here is a list of some Frequently Asked Questions.</p>
+			</div><!--addressDetails-->
 		</div><!--addressView-->
 
 		<div class="formDetails">
 			<form method="post" onsubmit="return validate()">
 				<label class="required">Enter Name</label>
-				<input type="text" name="strName" id="strName" placeholder="ex. Jane Doe" class="required" value="" required>
+				<input type="text" name="strName" id="strName" placeholder="Jane Doe" class="required" value="" required>
 				<label class="required">Enter Email</label>
-				<input type="text" name="strEmail" id="strEmail" placeholder="ex. abc@gmail.com" class="required" value="" required>
+				<input type="text" name="strEmail" id="strEmail" placeholder="abc@gmail.com" class="required" value="" required>
 				<label class="required">Enter Message</label>
-				<textarea type="textarea" name="strMessage" id="strMessage" placeholder="Any Specific requirements.." class="required" required></textarea>
-				<input type="submit" name="submit" value="Submit" class="btn btn-submit">
+				<textarea type="textarea" name="strMessage" id="strMessage" placeholder="Enter your message.." class="required" required></textarea>
+				<input type="submit" name="submit" value="Submit" class="btn btn-primary">
 			</form>
-		</div><!--formDetails--->
+		</div><!--formDetails-->
 		<div id="map"></div><!--map-->
 	</div><!--contactClass-->
 </div><!--ContactView-->
