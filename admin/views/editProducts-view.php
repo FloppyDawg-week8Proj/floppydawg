4cdc88e56e13ee3b57e844aa13b2a9d738a10e1a
VANARTS\icarillo <italo.carillo@globo.com>
2018-11-18 18:25:32 -0800

    update products working

--- admin/views/editProducts-view.php
+++ admin/views/editProducts-view.php
@@ -1,32 +1,125 @@
 <?php 
-$arrProducts = runSelectSQL("SELECT * FROM products WHERE id=".$_GET["id"]);
+$sql = 'SELECT * FROM sizes';
+$arrData['sizes'] = runSelectSQL($sql);
+
+$sql = 'SELECT * FROM categories';
+$arrData['categories'] = runSelectSQL($sql);
+
+$sql = 'SELECT * FROM advantages';
+$arrData['advantages'] = runSelectSQL($sql);
+
+$sql = "SELECT 
+			products.*
+		FROM 
+			products
+		WHERE products.id=".$_GET["id"];
+$productDetails = runSelectSQL($sql)[0];
+
+$sql = "SELECT 
+			product_advantages.nAdvantageID
+		FROM 
+			product_advantages
+		WHERE product_advantages.nProductID=".$_GET["id"];
+$results = runSelectSQL($sql);
+
+$productDetails['advantages'] = [];
+foreach($results as $result){
+	array_push($productDetails['advantages'], $result['nAdvantageID']);
+}
+
+
+$sql = "SELECT 
+			photos.strFile,
+			photos.bMainPhoto
+		FROM 
+			photos
+		WHERE photos.nProductID=".$_GET["id"];
+$results = runSelectSQL($sql);
+
+$productDetails['photos'] = [];
+foreach($results as $result){
+	array_push($productDetails['photos'], $result['strFile']);
+}
+
 ?>
 <form method="POST" action="views/updateProduct.php" class="addAproductForm" enctype="multipart/form-data">
-		<input type="hidden" name="id" value="<?=$arrProducts[0]['id']?>">
-		<h2>Edit a Product</h2>
-		<label class="required">Name of the Product</label>
-		<input type="text" name="strName" id = "strName" class="required" value="<?=$arrProducts[0]['strName']?>"/>
+		<input type='hidden' name="id" value="<?=$_GET['id']?>" />
+		
+		<h2>Edit Product</h2>
+		<label>Name of the Product</label>
+		<input type="text" name="strName" class="required" value="<?=$productDetails['strName']?>"/>
 
-		<label class="required">Price</label>
-		<input type="number" name="nPrice" class="required" value="<?=$arrProducts[0]['nPrice']?>"/>
+		<label>Price</label>
+		<input type="number" name="nPrice" class="required" value="<?=$productDetails['nPrice']?>"/>
 
+		<label class="required">Category</label>
+		<select name="nCategory" id="">
+			<?php
+			foreach($arrData['categories'] as $category){	
+				$selected = ($category['id']==$productDetails['nCategory'])?'selected':'';
+			?>
+			<option value='<?=$category['id'];?>' <?=$selected?>><?=$category['strName'];?></option>
+			<?php
+				}
+			?>
+		</select>
 
-		<label class="required">Size</label>
-		<input type="number" name="nSizeID" class="required" value="<?=$arrProducts[0]['nSizeID']?>"/>
+		<label>Size</label>
+		<select name="nSizeID" class="required">
+			<?php
+				foreach( $arrData['sizes']  as $size){
+					$selected = ($size['id']==$productDetails['nSizeID'])?'selected':'';
+			?>
+			<option value='<?=$size['id'];?>' <?=$selected?>><?=$size['strName'];?></option>
+			<?php
+				}
+			?>
+		</select>
 
-		<label class="required">Advantages</label>
-		<input type="textarea" name="strAdvantage" class="required" value="<?=$arrProducts[0]['strAdvantage']?>"/>
+		<label class="required" >Featured</label>
+		<input type="radio" name="bFeatures" value="1">Yes <br>
+		<input type="radio" name="bFeatures" value="0" checked>No<br>
 
-		<label class="required">Reviews</label>
-		<input type="number" name="nReviews" class="required" value="<?=$arrProducts[0]['nReviews']?>"/>
+		<label>Advantages</label>
+		<?php
+			foreach( $arrData['advantages']  as $advantage){
+				$checked = (in_array($advantage['id'], $productDetails['advantages']))?'checked':'';
+		?>
+		<div class="advantegeRow col-12">
+			<input type="checkbox" name="nAdvantageID[]" value='<?=$advantage['id'];?>' <?=$checked?>><?=$advantage['strName'];?>
+		</div>
+		<?php
+			}
+		?>
 
-		<label class="required">Stars</label>
-		<input type="number" name="nStars" class="required" value="<?=$arrProducts[0]['nStars']?>"/>
+		<label>Reviews</label>
+		<input type="number" name="nReviews" class="required" value="<?=$productDetails['nReviews']?>"/>
+		
+		<label>Stars</label>
+		<input type="range" name="nStars" class="required col-12" min="0" max="50" onchange='starsRange()' value='<?=$productDetails['nStars']?>'/>
+		<div class='starsHolder col-12'>
+			<span style="width: <?=$productDetails['nStars']*2?>%"></span>
+			<img src="../images/starMask.png" alt="stars Mask" class="starsMask col-12">
+		</div>
 
-		<label class="required">Description</label>
-		<input type="textarea" name="strDescription" class="required" value="<?=$arrProducts[0]['strDescription']?>"/>
+		<label>Description</label>
+		<textarea name="strDescription" class="required" value=""><?=$productDetails['strDescription']?></textarea>
+		
+		<label>Upload Photo</label>
+		<input id="myFile" type="file" name="strImage[]" multiple>
+		<div id="myImages" class="col-12">
+		<?php
+			foreach($productDetails['photos'] as $photo){
+		?>
+			<div class="product-view-holder">
+				<div class="product-pictures-preview coverBg">
+					<img class="col-12" src="../assets/<?=$photo?>" alt="defaultPhoto">
+				</div>
+			</div>
+		<?php } ?>
+		</div>
 
-		<input type="submit" value="Update Product">
+		<input type="hidden" name="bMainPhoto" value='0'/>
+		<input type="submit" value="Save Changes">
 		<a href="?page=products">Cancel</a>
-
-	</form><!--addAproductForm-->
+</form><!--addAproductForm-->
--- admin/views/updateProduct.php
+++ admin/views/updateProduct.php
@@ -1,17 +1,70 @@
 <?php
 include("../../../../functions/dbqueries.php");
 
-runSQL("
-	UPDATE
-		products
-	SET
-		strName = '".$_POST["strName"]."',
-		nPrice = '".$_POST["nPrice"]."',
-		nReviews = '".$_POST["nReviews"]."',
-		nStars = '".$_POST["nStars"]."',
-		strDescription = '".$_POST["strDescription"]."',
-		strAdvantage = '".$_POST["strAdvantage"]."',
-		nSizeID = '".$_POST["nSizeID"]."'
-		WHERE id=".$_POST["id"]);
- header("location: ../index.php?page=products");
+//update product information in products table
+$sql = "UPDATE 
+			products
+		SET
+			strName = '".$_POST["strName"]."',
+			nPrice = '".$_POST["nPrice"]."', 
+			nCategory = '".$_POST["nCategory"]."',
+			strDescription = '".$_POST["strDescription"]."',
+			nReviews = '".$_POST["nReviews"]."',
+			nStars = '".$_POST["nStars"]."', 
+			nSizeID = '".$_POST["nSizeID"]."',
+			bFeatures = '".$_POST["bFeatures"]."'
+		WHERE 
+			id=".$_POST["id"];
+runSQL($sql); //Update product
+
+
+//update product photos
+if(!empty($_FILES['strImage']['name'][0])){
+	$values = '';
+	$i = 0;
+	while($i < count($_FILES['strImage']["name"])){	
+		$bMainPhoto = ($i == $_POST['bMainPhoto'])?true:false;
+		$value = (empty($values)?"('".$_FILES['strImage']["name"][$i]."', '".$_POST["id"]."', '".$bMainPhoto."')":", ('".$_FILES['strImage']["name"][$i]."', '".$_POST['id']."', '".$bMainPhoto."')");
+		$values .= $value;
+		
+		saveImgOnServer($_FILES['strImage']["name"][$i], $_FILES["strImage"]["tmp_name"][$i]);//save photo inside assets folder
+
+		$i++;
+	}
+	
+	//Delete all old photos for this product
+	$sql = "DELETE FROM photos WHERE nProductID=".$_POST['id'];
+	runSQL($sql);//ther is a beter way to do this updating just the photo that you want but this current way is more simple
+	
+	
+	//Insert the new photos for this product
+	$sql = "INSERT INTO photos(
+		strFile,
+		nProductID,
+		bMainPhoto
+	)
+	VALUES ".$values;
+	runInsertSQL($sql); //relate photo and product (midle table)
+}
+if(!empty($_POST['nAdvantageID'])){
+	$values = '';
+	foreach($_POST['nAdvantageID'] as $advantage){
+		$value = (empty($values)?"('".$_POST['id']."', '".$advantage."')":", ('".$_POST['id']."', '".$advantage."')");
+		$values .= $value;
+	}
+	//Delete all old advantages for this product
+	$sql = "DELETE FROM product_advantages WHERE nProductID=".$_POST['id'];
+	runSQL($sql);//ther is a beter way to do this updating just the photo that you want but this current way is more simple
+
+
+	//Insert the new photos for this product
+	$sql = "INSERT INTO 
+				product_advantages(nProductID, nAdvantageID) 
+			VALUES 
+				".$values;
+	runInsertSQL($sql); //relate product and advantage
+}
+print_r($_FILES);
+die;
+//header("location: ../index.php?page=products");
 ?>
