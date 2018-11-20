ab62f158d77ff0e7031dbdf1015d7e874b2ee0b0
Abigail Ferguson <AFerguson@vanarts.com>
2018-11-19 21:54:55 -0800

    changed cart css

--- Views/cart-view.php
+++ Views/cart-view.php
@@ -1,21 +1,50 @@
-	<h2 class="page-title">Cart</h2>
+	<h2 class="cart-title">Your Cart</h2>
 	<?php if($arrData){ ?>
 	<form class="cart-form" method="post" action="submit-order.php">
 		<div class="cart-resume">
-			<?php
-				echo $arrData;
-			?>
-		</div>	
-		<div class="btn btn-primary">
-			<a href="#">Continue Shopping</a>
-		</div>
-		<div class="btn btn-grey">
-			<a href="index.php?controller=checkout&action=billing">Checkout</a>
-		</div>
+			<table id="cartTable" class='table'>
+				<thead>
+					<tr>
+						<th>Product</th>
+						<th>Name</th>
+						<th>Unit</th>
+						<th>Quantity</th>
+						<th>Total</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+					echo $arrData;
+				?>
+				</tbody>
+			</table>
+		</div><!--cart-resume-->
+
+		<div class="continue-shopping">
+			<a href="#" class="btn btn-secondary">Continue Shopping</a>
+		</div><!--continue-shopping-->
+
+		<div class="subtotal">
+			<p class="subtotal-price">Subtotal: $90.95</p>
+			<p class="free-shipping">Free shipping</p>
+		</div><!--subtotal-->
+
+		<div class="cart-bottom-btns">
+			<img src="assets/trust-signs.png" alt="trust-signs" class="trust-signs">
+
+			<a href="index.php?controller=checkout&action=billing" class="btn btn-primary checkout">Check Out</a>
+
+			<a href="#">
+				<img src="assets/paypal.png" alt="paypal" class="paypal">
+			</a>
+		</div><!--cart-bottom-btns-->
 	</form>
-	<?php }else { ?>
+
+	<?php }
+	else { ?>
 		<h3>No products in the cart</h3>	
-		<div class="btn btn-primary">
+		<div class="btn btn-secondary">
 			<a href="index.php?action=products">Continue Shopping</a>
 		</div>
 	<?php } ?>
